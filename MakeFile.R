dd06375ea8692b387b9729c2c07407b473a8d984
Vatche Baboyan <vatchebaboyan@vatches-mbp.home>
2019-11-30 18:46:09 -0800

    splash

---
+++ MakeFile.R
@@ -0,0 +1,21 @@
+library(knitr)
+library(brocks)
+
+#just filename not path
+filename <- commandArgs(trailingOnly = TRUE)
+setwd("/Users/vatchebaboyan/Dropbox/Blog/vbaboyan.github.io")
+#opts_chunk$set(comment= NA, fig.path='/images/')
+output<- paste0(getwd(),"/_posts/",Sys.Date(),"-", sub('.Rmd', '.html', filename))
+
+# Check that the input is an .Rmd file ----
+if(!grepl(".Rmd", filename)) {	
+  stop("You must specify a .Rmd file.")
+}
+
+knit(input= paste0("_drafts/",filename),
+     output=output,
+     encoding= 'UTF-8')
+
+# Create the dependencies ----
+htmlwidgets_deps(a= paste0(Sys.Date(), "-", filename), 
+                 always = T)
